e29f162ee9c1895ec9c10008658a5035d6ae66ff
juanAdso <juan18sepulveda19@gmail.com>
2026-08-27 17:31:05 -0500

    modelo categorias

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    /** @use HasFactory<\Database\Factories\CategoriaFactory> */
-    use HasFactory;
+    protected $table = 'categorias';
+
+    protected $primaryKey = 'id_categorias';
+
+    protected $fillable = [
+        'nombre_categoria',
+        'descripcion',
+        'estado',
+    ];
+
 }
